908eb4839604afe17801e023440803dd30f1a670
Sile <billtaosile@hotmail.com>
2023-05-04 20:46:32 -0400

    remove comments

--- model.py
+++ model.py
@@ -126,18 +126,13 @@ def fit(net, optimizer, y_train, y_eval, train_dataloader, val_dataloader, num_e
         # plt.show()
     return train_loss_epoch_arr, val_loss_epoch_arr, train_acc_epoch_arr, val_acc_epoch_arr
 
+
 transform_train = transforms.Compose([
-    # Randomly crop the image to obtain an image with an area of 0.08 to 1 of
-    # the original area and height-to-width ratio between 3/4 and 4/3. Then,
-    # scale the image to create a new 224 x 224 image
     transforms.RandomResizedCrop(IMAGE_SIZE, scale=(0.08, 1.0),
                                  ratio=(3.0 / 4.0, 4.0 / 3.0)),
     transforms.RandomHorizontalFlip(),
-    # Randomly change the brightness, contrast, and saturation
     transforms.ColorJitter(brightness=0.4, contrast=0.4, saturation=0.4),
-    # Add random noise
     transforms.ToTensor(),
-    # Standardize each channel of the image
     transforms.Normalize(RGB_MEAN, RGB_STD)])
 
 transform_test = transforms.Compose([
